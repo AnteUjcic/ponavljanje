81854502392eecf3fb77bb4300b3b9baef6e4917
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2019-12-25 12:56:23 +0100

    Add files via upload

---
+++ diagonala kvadrata.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include <cstdlib> 
+#include <ctime>
+using namespace std;
+int main() {
+//	int peppa_pig[5][5],
+int x,y;
+
+	cout << "upisite broj za velicinu x osi: ";
+	cin >> x;	
+//	peppa_pig[x];
+	 int i,o,p,q=1;
+	
+	 for (i = 2;i <= x;i++) {
+		 p = p + x+1;
+		   q = q + p;
+
+		 }
+	 cout << q;
+
+
+
+	}
+
