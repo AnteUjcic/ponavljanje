6236a80134fc5b3be50f7599ca7905c994b0f3a9
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2019-12-25 13:00:12 +0100

    Update diagonala kvadrata.cpp

--- diagonala kvadrata.cpp
+++ diagonala kvadrata.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
 //	int peppa_pig[5][5],
 int x,y;
-
-	cout << "upisite broj za velicinu x osi: ";
+ 
+	cout << "upisite broj za velicinu matrice npr ako stavite 5 matrice ce biti 5X5: ";
 	cin >> x;	
 //	peppa_pig[x];
 	 int i,o,p,q=1;
@@ -18,7 +18,7 @@ int x,y;
 		 }
 	 cout << q;
 
-
-
+system("pause");
+return 0;
 	}
 
